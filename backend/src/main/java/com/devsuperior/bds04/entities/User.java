b86bddd4098edf136c1b1bb35ab720c51fbc9d5b
Rafaelxndre <rafaelxndre@gmail.com>
2022-01-25 18:09:43 -0300

    create UserService, exceptions, configurações iniciais

--- backend/src/main/java/com/devsuperior/bds04/entities/User.java
+++ backend/src/main/java/com/devsuperior/bds04/entities/User.java
@@ -6,9 +6,13 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -24,6 +28,10 @@ public class User implements Serializable{
 	private String email;
 	private String password;
 	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set <Role> roles = new HashSet<>();
 	
 	public User() {
