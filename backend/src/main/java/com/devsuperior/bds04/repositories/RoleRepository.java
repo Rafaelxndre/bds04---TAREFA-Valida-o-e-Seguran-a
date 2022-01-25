f3fc9ce5c9a5f7d947dfb3642307f225bd1db85c
Rafaelxndre <rafaelxndre@gmail.com>
2022-01-24 21:10:03 -0300

    Create Repositories User and Role

---
+++ backend/src/main/java/com/devsuperior/bds04/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.bds04.repositories;
+
+import javax.management.relation.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
